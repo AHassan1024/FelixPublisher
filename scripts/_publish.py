ad107f0d6580c622e6e05725835978e3b2cc729a
Ameena Hassan <ameenahassan1024@gmail.com>
2022-01-16 13:31:19 +0000

    <to revert> Something's gone wrong in this .py

--- scripts/_publish.py
+++ scripts/_publish.py
@@ -2,6 +2,9 @@
 import sys
 import os
 from os import path
+# Requires PyPDF2 through pip install PyPDF2 in main directory
+# > Successfully installed PyPDF2-1.26.0
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 def find_ext(folder, ext):
@@ -15,6 +18,7 @@ def find_ext(folder, ext):
 
 
 def create_directory(file, issue_number):
+    print("Creating empty folder called %s!" % issue_number)
     parent_dir = file[0:file[0:file.rfind("/")].rfind("/")]
     print(parent_dir)
     try:
@@ -22,6 +26,21 @@ def create_directory(file, issue_number):
         print("Directory '%s' created" % issue_number)
     except OSError as error:
         print(error)
+
+
+def split_and_save(file_path, issue_number):
+    print("Now splitting felix_%s.pdf" % issue_number)
+    full_pdf = PdfFileReader(file_path)
+    for page in range(full_pdf.getNumPages()):
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(full_pdf.getPage(page))
+        if page < 8:
+            output_filename = 'PDFE-E01-S2-00{}.pdf'.format(page + 1)
+        else:
+            output_filename = 'PDFE-E01-S2-0{}.pdf'.format(page + 1)
+        with open(working_dir.join(output_filename), 'wb') as out:
+            pdf_writer.write(out)
+        print('Created: {}'.format(output_filename))
 
 
 def find_pdf(folder):
@@ -32,29 +51,25 @@ def find_pdf(folder):
     pdfs = find_ext(folder, "pdf")
     # Scrape to find xxxx
     for f in pdfs:
+        f = pdfs[-1]
         issue_num = f[f.find("/felix_")+7:-4]
         issue_confirmation = input(
-            "I have detected " + issue_num + " to be the issue number. Please confirm this is correct (y/n): ")
-        if issue_confirmation == 'n':
-            issue_num = input(
-                "Please input your correct issue number, but be warned that it is not the issue number I detected. Please check that the Book is named felix_xxxx.pdf. : ")
-        elif issue_confirmation == 'y':
-            # TODO: now that issue_number is confirmed, create a folder , that contains the split up pages
-            create_directory(f, issue_num)
-            # split_and_save(f, issue_num)
+            "I have detected " + issue_num + " to be the issue number. Please confirm this is correct (y/n): ").lower() in ['y', "yup", "yes"]
+        if not issue_confirmation:
+            print("I was unable to detect the correct issue number. Please check the Final Book pdf is named 'felix_xxxx.pdf'")
+            sys.exit(1)
 
             # Go back to the menu now
-            continue
-
         else:
-            print("Error message, try to reroute this upwards...")
-            # TODO while loop here? while issue_conf != 'y'
+            print("Issue %s confirmed!" % issue_num)
+
     print('Done')
 
     # use pdfReader and pdf Writer to seperate the pdf
     # Rename the pdfs
 
 # Create new folder in Desktop with issue number as the name
+
 
     ###############################
 if __name__ == "__main__":
@@ -66,6 +81,7 @@ if __name__ == "__main__":
 
     # working_dir for testing should be pwd/../tests/files
     working_dir = sys.argv[1]
+    issue_num = 0
 
     while True:
         print()
@@ -86,4 +102,12 @@ if __name__ == "__main__":
             sys.exit(0)
 
         elif command == "1":
-            find_pdf(working_dir)
+            success = False
+            f = find_pdf(working_dir)
+            if (issue_num != 0):
+                create_directory(working_dir, issue_num)
+                split_and_save(f, issue_num)
+            # success = True
+            print(success)
+        else:
+            print("Apologies, this option is not yet supported.")
